e771e81bf7bbad73eb3bf24bef1116813ee0fe5a
gmarler <gmarler@gmarler.com>
2015-07-24 21:09:30 -0400

    MetricSet begins

--- lib/PA/MetricSet.pm
+++ lib/PA/MetricSet.pm
@@ -3,4 +3,26 @@ use warnings;
 
 package PA::MetricSet;
 
+# VERSION
+#
+# ABSTRACT: Encapsulates a set of PA::Metric's
+
+use namespace::autoclean;
+use Moose;
+
+# metrics indexed by module name
+has 'modules' =>
+  ( is       => 'ro',
+    isa      => 'HashRef',
+  );
+
+# metrics indexed by host name
+has 'byhost' =>
+  ( is       => 'ro',
+    isa      => 'HashRef',
+  );
+
+
+__PACKAGE__->meta->make_immutable;
+
 1;
