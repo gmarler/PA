d9266e2b5746a1bf31af3ac6655bba8afea1cc18
gmarler <gmarler@gmarler.com>
2015-07-26 11:48:52 -0400

    Start testing setup for PA::Metric

---
+++ t/TF/PA/Metric.pm
@@ -0,0 +1,20 @@
+# NOTE: TF stands for TestsFor::...
+package TF::PA::Metric;
+
+use Time::Moment      qw();
+use File::Temp        qw();
+use Data::Dumper      qw();
+
+use Test::Class::Moose;
+with 'Test::Class::Moose::Role::AutoUse';
+
+# Set up for schema
+# BEGIN { use PA::Schema; }
+
+sub test_startup {
+  my ($test, $report) = @_;
+  $test->next::method;
+
+  # ... Anything you need to do...
+}
+
