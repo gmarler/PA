614c6d4b01b8eadcbefaae22568b0051bccdd7a5
gmarler <gmarler@gmarler.com>
2015-07-26 11:55:55 -0400

    reorganize t lib dir correctly

---
+++ t/lib/TF/PA/MetadataManager.pm
@@ -0,0 +1,26 @@
+# NOTE: TF stands for TestsFor::...
+package TF::PA::MetadataManager;
+
+use Time::Moment      qw();
+use File::Temp        qw();
+use Data::Dumper      qw();
+
+use Test::Class::Moose;
+with 'Test::Class::Moose::Role::AutoUse';
+
+# Set up for schema
+# BEGIN { use PA::MetadataManager::Schema; }
+
+sub test_startup {
+  my ($test, $report) = @_;
+  $test->next::method;
+
+  # ... Anything you need to do...
+}
+
+sub test_normal {
+  my ($test) = shift;
+
+  
+}
+
