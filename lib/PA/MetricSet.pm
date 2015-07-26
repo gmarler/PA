face7eb2bed9f09952dc37fa6dc14b3d8e4e69f0
gmarler <gmarler@gmarler.com>
2015-07-25 23:46:12 -0400

    A couple of MetricSet methods added

--- lib/PA/MetricSet.pm
+++ lib/PA/MetricSet.pm
@@ -23,6 +23,57 @@ has 'byhost' =>
   );
 
 
+=method add_metric
+
+Add a metric from the form used by profile and metric metadata, which is
+an object with just "module", "stat" and "fields".
+
+=cut
+
+sub add_metric {
+  my ($self, $module, $stat, $fields) = @_;
+
+  my ($statfields);
+
+  if (not any { /^ $module $/x } keys %{$self->modules}) {
+    $self->modules->{module} = {};
+  }
+
+  if (not any { /^ $stat $/x } keys %{$self->modules->{$module}}) {
+    $self->modules->{$module}->{$stat} = {};
+  }
+
+  $statfields = $self->modules->{$module}->{$stat};
+
+  for (my $i = 0; $i < scalar(@$fields); $i++) {
+    $statfields->{$fields->[$i]} = 1;
+  }
+}
+
+=method base_metric
+
+Returns a metric instance for the metric identified by the specified
+module and stat, if such a metric exists in this set.  Returns undef
+otherwise.
+
+=cut
+
+sub base_metric {
+  my ($self, $module, $stat) = @_;
+
+  if (not any { /^ $module $/x } keys %{$self->modules}) {
+    return;
+  }
+
+  if (not any { /^ $stat $/x } keys %{$self->modules->{$module}}) {
+    return;
+  }
+
+  return Metric->new($module, $stat, $self->modules->{$module}->{$stat});
+}
+
+
+
 __PACKAGE__->meta->make_immutable;
 
 1;
