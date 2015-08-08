d97c85cfc3a2e50068c009bde470ef26933b1135
gmarler <gmarler@gmarler.com>
2015-08-08 13:19:15 -0400

    More PAP work

--- lib/PA/AMQP/PAP.pm
+++ lib/PA/AMQP/PAP.pm
@@ -18,9 +18,51 @@ with 'MooseX::Log::Log4perl';
 #############################################################################
 
 has [ 'amqp_prefix' ] => (
-  is       => 'rw',
+  is       => 'ro',
   isa      => 'Str',
   builder  => '_build_amqp_prefix',
+);
+
+# If configured, we may ping ourselves at the specified interval to detect
+# rabbitmq broker disconnection.
+has [ 'amqp_ping_interval' ] => (
+  is       => 'ro',
+  isa      => 'Str',
+  default  => sub {
+    my $self = shift;
+    return 30 * 1000;  # 30 secs
+  },
+);
+
+# The Performance Analytics Protocol API is versioned with a major and minor
+# number.
+# Software components should ignore messages received with a newer major
+# version number.
+has [ 'amqp_vers_major' ] => (
+  is       => 'ro',
+  isa      => 'Num',
+  default  => 2,
+);
+
+has [ 'amqp_vers_minor' ] => (
+  is       => 'ro',
+  isa      => 'Num',
+  default  => 5,
+);
+
+# We only use one global exchange of type 'direct'
+has [ 'amqp_exchange' ] => (
+  is       => 'ro',
+  isa      => 'Str',
+  default  => 'amq.direct',
+);
+
+has [ 'amqp_exchange_opts' ] => (
+  is       => 'ro',
+  isa      => 'HashRef',
+  default  => sub {
+    return { type => 'direct', };
+  }
 );
 
 =head1 SYNOPSIS
