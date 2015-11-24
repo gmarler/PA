58495e7b31423406761e7281a7c6b4f87162166a
Gordon Marler <gmarler@gmarler.com>
2015-11-24 15:34:59 -0500

    Set default_resultset_class

--- lib/PA/Schema.pm
+++ lib/PA/Schema.pm
@@ -1,5 +1,6 @@
 package PA::Schema;
 
+use 5.20.0;
 use strict;
 use warnings;
 
@@ -7,6 +8,8 @@ use base 'DBIx::Class::Schema';
 
 our $VERSION = 2;
 
-__PACKAGE__->load_namespaces;
+__PACKAGE__->load_namespaces(
+  default_resultset_class => 'ResultSet',
+);
 
 1;
