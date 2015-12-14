55771afb40037ae272789e26cc28585dd8a1eef8
Gordon Marler <gmarler@gmarler.com>
2015-12-14 12:38:25 -0500

    arcstat hit rate calculation confirmed

--- spikes/arcstats_rate_test.sql
+++ spikes/arcstats_rate_test.sql
@@ -1,15 +1,16 @@
-SELECT prev_timestamp, timestamp, prev_hits, hits,
-       (hits - prev_hits) AS hit_diff,
-       ((timestamp - prev_timestamp) ) AS timestamp_diff,
-       ((hits - prev_hits) / ((timestamp - prev_timestamp) / 1000000000)) AS rate_per_sec
+SELECT ((hits - prev_hits) / ( (snaptime::float - prev_snaptime::float) / 1000000000.0 ))::bigint AS hit_rate_per_sec
 FROM
-  (SELECT timestamp, hits,
-          lag(hits) OVER (ORDER BY timestamp ASC) as prev_hits,
-          lag(timestamp) OVER (ORDER BY timestamp ASC) as prev_timestamp
+  (SELECT snaptime, hits,
+          lag(hits) OVER (ORDER BY snaptime ASC) as prev_hits,
+          lag(snaptime) OVER (ORDER BY snaptime ASC) as prev_snaptime
    FROM
-     rate
+     arcstat
    ORDER BY
-     timestamp ASC) as w1;
+     snaptime ASC) as w1
+WHERE
+  (hits - prev_hits) > 0 AND
+  (snaptime - prev_snaptime) > 0
+  ;
 
 
 
