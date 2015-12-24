fd73edf991568da42d00853290755646b3e95875
Gordon Marler <gmarler@gmarler.com>
2015-12-23 22:51:30 -0500

    Add scope values for hosts and first subsystem

--- root/app/pa-app.js
+++ root/app/pa-app.js
@@ -3,5 +3,14 @@ angular.module('PA', [
   'host',
   'subsystems'
 ]).controller('MainCtrl', function ($scope) {
+  $scope.hosts = [
+    {"id": 1, "name": "nydevsol10", "time_zone": "America/New_York"},
+    {"id": 2, "name": "sundev51", "time_zone": "America/New_York"},
+    {"id": 3, "name": "p315", "time_zone": "Europe/London"},
+    {"id": 4, "name": "solperf1", "time_zone": "America/New_York"}
+  ];
 
+  $scope.subsystems = [
+    {"name": "CPU"}
+  ];
 });
