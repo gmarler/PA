24366bef4ec11bdb73dae58f2e147d9a68b85c34
Gordon Marler <gmarler@gmarler.com>
2015-12-23 23:14:56 -0500

    Get first ui-route test working.

--- root/app/pa-app.js
+++ root/app/pa-app.js
@@ -2,15 +2,25 @@ angular.module('PA', [
   'ui.router',
   'host',
   'subsystems'
-]).controller('MainCtrl', function ($scope) {
-  $scope.hosts = [
-    {"id": 1, "name": "nydevsol10", "time_zone": "America/New_York"},
-    {"id": 2, "name": "sundev51", "time_zone": "America/New_York"},
-    {"id": 3, "name": "p315", "time_zone": "Europe/London"},
-    {"id": 4, "name": "solperf1", "time_zone": "America/New_York"}
-  ];
+])
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('PA', {
+        url:'',
+        templateUrl: 'subsystems/subsystems.tmpl.html',
+        controller: 'MainCtrl'
+      })
+    ;
+  })
+  .controller('MainCtrl', function ($scope) {
+    $scope.hosts = [
+      {"id": 1, "name": "nydevsol10", "time_zone": "America/New_York"},
+      {"id": 2, "name": "sundev51", "time_zone": "America/New_York"},
+      {"id": 3, "name": "p315", "time_zone": "Europe/London"},
+      {"id": 4, "name": "solperf1", "time_zone": "America/New_York"}
+    ];
 
-  $scope.subsystems = [
-    {"name": "CPU"}
-  ];
+    $scope.subsystems = [
+      {"name": "CPU"}
+    ];
 });
