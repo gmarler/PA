215f882b0779febd39197e799a995ace385f982e
gmarler <gmarler@gmarler.com>
2015-12-31 09:06:52 -0500

    Initial protractor test config and page to run it on

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,7 +1,9 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['root/spec/e2e/**/*.js'],
   jasmineNodeOpts: {
     showColors: true, // use colors in the command line report
     defaultTimeoutInterval: 30000
-  }
+  },
+  baseUrl: 'http://localhost:8080/'
 };
