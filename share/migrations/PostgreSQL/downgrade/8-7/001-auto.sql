ddd47fc3a8ce0d3f71b07d1c66a4d1da1444f8ff
Gordon Marler <gmarler@gmarler.com>
2016-01-05 13:48:53 -0500

    Add v8 Schema migration

---
+++ share/migrations/PostgreSQL/downgrade/8-7/001-auto.sql
@@ -0,0 +1,5 @@
+-- Convert schema '/perfwork/gitwork/PA/share/migrations/_source/deploy/8/001-auto.yml' to '/perfwork/gitwork/PA/share/migrations/_source/deploy/7/001-auto.yml':;
+
+;
+-- No differences found;
+
