92218421d80d3d4e892a391c19ff27734cb70cc7
Gordon Marler <gmarler@gmarler.com>
2016-01-06 17:56:17 -0500

    Alter Memstat class to simplify it, with a type column, rather than a row for every possible statistic

--- lib/PA/Schema/Result/Memstat.pm
+++ lib/PA/Schema/Result/Memstat.pm
@@ -20,109 +20,25 @@ __PACKAGE__->add_columns(
     data_type => 'timestamptz',
     timezone => 'UTC',
   },
-  #
-  # Groups of 3
-  #
-  'free_cachelist_bytes'  => {
-    data_type         => 'bigint',
-  },
-  'free_cachelist_page_count'  => {
-    data_type         => 'integer',
-  },
-  'free_cachelist_pct_of_total'  => {
-    data_type         => 'smallint',
-  },
-  #
-  'defdump_prealloc_bytes'  => {
-    data_type         => 'bigint',
-  },
-  'defdump_prealloc_page_count'  => {
-    data_type         => 'integer',
-  },
-  'defdump_prealloc_pct_of_total'  => {
-    data_type         => 'smallint',
-  },
-  #
-  'exec_and_libs_bytes'  => {
-    data_type         => 'bigint',
-  },
-  'exec_and_libs_page_count'  => {
-    data_type         => 'integer',
-  },
-  'exec_and_libs_pct_of_total'  => {
-    data_type         => 'smallint',
-  },
-  #
-  'free_freelist_bytes'  => {
-    data_type         => 'bigint',
-  },
-  'free_freelist_page_count'  => {
-    data_type         => 'integer',
-  },
-  'free_freelist_pct_of_total'  => {
-    data_type         => 'smallint',
-  },
-  #
-  'zfs_file_data_bytes'  => {
-    data_type         => 'bigint',
-  },
-  'zfs_file_data_page_count'  => {
-    data_type         => 'integer',
-  },
-  'zfs_file_data_pct_of_total'  => {
-    data_type         => 'smallint',
-  },
-  #
-  'anon_bytes'  => {
-    data_type         => 'bigint',
-  },
-  'anon_page_count'  => {
-    data_type         => 'integer',
-  },
-  'anon_pct_of_total'  => {
-    data_type         => 'smallint',
-  },
-  #
-  'page_cache_bytes'  => {
-    data_type         => 'bigint',
-  },
-  'page_cache_page_count'  => {
-    data_type         => 'integer',
-  },
-  'page_cache_pct_of_total'  => {
-    data_type         => 'smallint',
-  },
-  #
-  'zfs_metadata_bytes'  => {
-    data_type         => 'bigint',
-  },
-  'zfs_metadata_page_count'  => {
-    data_type         => 'integer',
-  },
-  'zfs_metadata_pct_of_total'  => {
-    data_type         => 'smallint',
-  },
-  #
-  'kernel_bytes'  => {
-    data_type         => 'bigint',
-  },
-  'kernel_page_count'  => {
-    data_type         => 'integer',
-  },
-  'kernel_pct_of_total'  => {
-    data_type         => 'smallint',
-  },
-  #
-  'total_bytes'  => {
-    data_type         => 'bigint',
-  },
-  'total_page_count'  => {
-    data_type         => 'integer',
-  },
-  'total_pct_of_total'  => {
-    data_type         => 'smallint',
-  },
 
+  #
+  # The type of this memstat data row; this is an enumeration of the following:
+  # free_cachelist, defdump_prealloc, exec_and_libs, free_freelist,
+  # zfs_file_data, anon, page_cache, zfs_metadata, kernel, total
+  #
+  'type'   => {
+    data_type         => 'varchar(16)',
+  }
+  #
+  'bytes'  => {
+    data_type         => 'bigint',
+  },
+  'page_count'  => {
+    data_type         => 'integer',
+  },
+  'pct_of_total'  => {
+    data_type         => 'smallint',
+  },
 );
 
 __PACKAGE__->set_primary_key('memstat_id');
