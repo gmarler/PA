53e348d99b4bd9d6f7947b2e2ab4b68d74024dc4
gmarler <gmarler@gmarler.com>
2016-02-28 18:38:47 -0500

    More tests for PA home page - getFirstHostname does not yet work.

--- root/spec/e2e/spec.js
+++ root/spec/e2e/spec.js
@@ -18,17 +18,43 @@ var PAHomepage = function() {
   this.getTitle = function() {
     return element(by.css('title'));
   };
+
+  this.getHostInfo = function() {
+    return element(by.css('div[host-info]'));
+  };
+
+  this.getHostInfoBrand = function() {
+    return element(by.css('div[host-info] > nav[role="navigation"] > div.navbar-header > a.navbar-brand'));
+  };
+
+  this.getFirstHostname = function() {
+    var first_hostname = element(by.repeater('host in vm.hosts').row(0));
+
+    console.log(first_hostname);
+    return first_hostname;
+  };
 };
 
 describe('PA HomePage', function() {
+  var pahomepage    = new PAHomepage();
+  var ExpectedTitle = "Performance Analytics for Solaris";
+
+  pahomepage.get();
+
   it('should have the expected title', function() {
-    var ExpectedTitle = "Performance Analytics for Solaris";
-
-    var pahomepage = new PAHomepage();
-
-    pahomepage.get();
-
     expect(pahomepage.getTitle().getInnerHtml()).toEqual(ExpectedTitle);
     expect(pahomepage.getTitle().getAttribute("text")).toEqual(ExpectedTitle);
+  });
+
+  it('should have HostInfo navbar', function() {
+    expect(pahomepage.getHostInfo().toBeDefined);
+  });
+
+  it('should have proper navbar branding title', function() {
+    expect(pahomepage.getHostInfoBrand().getAttribute("text").toBeDefined);
+  });
+
+  it('should have a list of hosts in dropdown menu', function() {
+    expect(pahomepage.getFirstHostname()).toEqual('nydevsol10');
   });
 });
