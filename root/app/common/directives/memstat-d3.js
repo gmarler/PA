b260dc8b8e84f45cdd0c70e6de8eb331ea9648e9
gmarler <gmarler@gmarler.com>
2016-03-04 18:15:53 -0500

    First working D3 directive for memstat - completely hard coded.

--- root/app/common/directives/memstat-d3.js
+++ root/app/common/directives/memstat-d3.js
@@ -16,15 +16,132 @@
     return directive;
 
     function link(scope,element,attrs) {
-      scope.selectedHostName = "Select Hostname";
-      scope.selectedHostTimeZone = "N / A";
-      scope.selectedHostId = undefined;
+      var margin = {top: 20, right: 20, bottom: 30, left: 50};
+      var width  = parseInt(d3.select('body').style('width'));
+      width      = width - margin.left - margin.right;
+      // var height = parseInt(d3.select('body').style('height'));
+      var height = 1000;
+      height     = height - margin.top - margin.bottom;
 
-      scope.hostSelected = function (hostobj) {
-        scope.selectedHostName = hostobj.name;
-        scope.selectedHostTimeZone = hostobj.time_zone;
-        scope.selectedHostId = hostobj.id;
-      };
+      var formatPercent = d3.format(".0%");
+
+      var x = d3.time.scale()
+                .range([0, width]);
+
+      var y = d3.scale.linear()
+                .range([height, 0]);
+
+      var color = d3.scale.category20();
+
+      var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+      var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .tickFormat(formatPercent);
+
+      var area = d3.svg.area()
+        .x(function(d) { return x(d.timestamp); })
+        .y0(function(d) { return y(d.y0); })
+        .y1(function(d) { return y(d.y0 + d.y); });
+
+      var stack = d3.layout.stack()
+        .values(function(d) {
+          // console.log(d.values);
+          return d.values;
+        });
+
+      var svg = d3.select(element[0]).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      d3.json("data/memstat.json", function(error, data) {
+        if (error) throw error;
+
+        // Don't graph:
+        // - The Epoch timestamp
+        // - The Guest data (since it's currently always 0)
+        // - The Total
+        color.domain(d3.keys(data[0])
+          .filter(function(key) {
+            return (key !== "timestamp" && key !== "total_bytes" && key !== "guest"); }));
+
+        data.forEach(function(d) {
+          d.timestamp = new Date((d.timestamp * 1000));
+          // console.log(d);
+        });
+
+        var memtypes = stack(color.domain().map(function(name) {
+          console.log(name);
+          return {
+            name: name,
+            values: data.map(function(d) {
+              return {timestamp: d.timestamp, y: d[name] / d.total_bytes };
+            })
+          };
+        }));
+
+        x.domain(d3.extent(data, function(d) { return d.timestamp; }));
+
+        var memtype = svg.selectAll(".memtype")
+          .data(memtypes)
+          .enter().append("g")
+          .attr("class", "memtype");
+
+
+        var paths =
+          memtype.append("path")
+            .attr("class", "area")
+            .attr("d", function(d) { return area(d.values); })
+            .style("fill", function(d) { return color(d.name); });
+
+        // Highlight / unhighlight individual areas in the stack as we pass into and out of them
+        paths
+          .on('mouseover', function (d) {
+            d3.select(this)
+              .attr('stroke-width', 3)
+              .attr('fill', d3.rgb(color(d.name)).brighter())
+              .attr('stroke', color(d.name));
+          })
+          .on('mouseout', function(d) {
+            d3.select(this)
+              .attr('stroke-width', 0)
+              .attr('fill', color(d.name));
+          });
+
+
+        memtype.append("text")
+          .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+          .attr("transform", function(d) { return "translate(" + x(d.value.timestamp) + "," + y(d.value.y0 + d.value.y / 2) + ")"; })
+          .attr("x", -600)
+          .attr("dy", ".35em")
+          .attr('font-size', '14px')
+          .text(function(d) {
+            var regex1 = /^(.)/;
+            var regex2 = /_/g;
+            var regex3 = /zfs/gi
+            var cvt_name = d.name;
+            cvt_name = cvt_name.replace(regex1,function (s) { return s.toUpperCase(); });
+            cvt_name = cvt_name.replace(regex2, " ");
+            // cvt_name = cvt_name.replace(/\_(.)/g, function (s = $1) { return s.toUpperCase() });
+            cvt_name = cvt_name.replace(regex3,function (s) { return s.toUpperCase(); });
+            return cvt_name;
+          });
+
+        svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+
+        svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis);
+      });
+
     }
   }
 
