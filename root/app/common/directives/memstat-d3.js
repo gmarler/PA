50a7c2d08040ffc8aa861f66d982e0945ba152cc
Gordon Marler <gmarler@gmarler.com>
2016-03-05 00:02:08 -0500

    Correct D3 axis CSS, and D3 SVG width

--- root/app/common/directives/memstat-d3.js
+++ root/app/common/directives/memstat-d3.js
@@ -54,7 +54,7 @@
         });
 
       var svg = d3.select(element[0]).append("svg")
-        .attr("width", width + margin.left + margin.right)
+        .attr("width", width)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
