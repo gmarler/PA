ecf081b9f9b734d185aae736cf70663e79ebd454
Gordon Marler <gmarler@gmarler.com>
2016-03-06 09:54:36 -0500

    Start working on making Subsystems navbar appear only when a host is selected.

--- root/app/host/host.js
+++ root/app/host/host.js
@@ -32,11 +32,23 @@
       scope.selectedHostTimeZone = "N / A";
       scope.selectedHostId       = undefined;
 
+      // currently available list of subsystems with info for the current host
+      scope.availSubsystems      = undefined;
+
       scope.hostSelected = function (hostobj) {
+        // Reset list of available subsystems
+        scope.availSubsystems      = undefined;
+
+        // set up info related to the host just selected
         scope.selectedHostName     = hostobj.name;
         scope.selectedHostTimeZone = hostobj.time_zone;
         scope.selectedHostId       = hostobj.id;
       };
+
+      scope.availSubsystemsForHost = function(hostobj) {
+        // Mock up for now
+        scope.availSubsystems = [ 'Memory' ];
+      }
     }
   }
 
