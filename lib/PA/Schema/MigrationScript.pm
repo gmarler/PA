9e7b6d9a81b108f2e9591005d0b847208430726d
Gordon Marler <gmarler@gmarler.com>
2016-04-22 13:01:35 -0400

    Add integration script for DBIx::Class::Migration

---
+++ lib/PA/Schema/MigrationScript.pm
@@ -0,0 +1,34 @@
+package PA::Schema::MigrationScript;
+
+use strict;
+use warnings;
+use v5.20;
+
+# Taken from
+# https://metacpan.org/pod/distribution/DBIx-Class-Migration/lib/DBIx/Class/Migration/Tutorial/Catalyst.pod,
+# which shows how to integrate DBIx::Class::Migration with Catalyst's
+# ConfigLoader
+#
+
+use Moose;
+use PA::Web;
+
+extends 'DBIx::Class::Migration::Script';
+
+=method defaults
+
+Set the schema to use to be from Catalyst's idea of the Schema
+
+=cut
+
+sub defaults {
+  schema => PA::Web->model('Schema')->schema,
+}
+
+
+__PACKAGE__->meta->make_immutable;
+__PACKAGE__->run_if_script;
+
+
+1;
+
