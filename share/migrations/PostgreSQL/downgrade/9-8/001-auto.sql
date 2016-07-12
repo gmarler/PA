25e479351e5539bace01f2cc8e0795a11925281b
Gordon Marler <gmarler@gmarler.com>
2016-07-12 13:30:53 -0400

    add fixtures/migrations for Schema v9

---
+++ share/migrations/PostgreSQL/downgrade/9-8/001-auto.sql
@@ -0,0 +1,15 @@
+-- Convert schema '/home/gmarler/gitwork/PA/share/migrations/_source/deploy/9/001-auto.yml' to '/home/gmarler/gitwork/PA/share/migrations/_source/deploy/8/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+DROP TABLE timeseriesgap CASCADE;
+
+;
+DROP TABLE fsoplatjson CASCADE;
+
+;
+
+COMMIT;
+
---
+++ share/migrations/PostgreSQL/upgrade/8-9/001-auto.sql
@@ -0,0 +1,30 @@
+-- Convert schema '/home/gmarler/gitwork/PA/share/migrations/_source/deploy/8/001-auto.yml' to '/home/gmarler/gitwork/PA/share/migrations/_source/deploy/9/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+CREATE TABLE "fsoplatjson" (
+  "fsoplatjson_id" serial NOT NULL,
+  "host_fk" integer NOT NULL,
+  "timestamp" timestamptz NOT NULL,
+  "interval_data" jsonb NOT NULL,
+  PRIMARY KEY ("fsoplatjson_id")
+);
+CREATE INDEX "fsoplatjson_idx_host_fk" on "fsoplatjson" ("host_fk");
+
+;
+CREATE TABLE "timeseriesgap" (
+  "entity_fk" integer NOT NULL,
+  "timestamp" timestamptz NOT NULL,
+  "value" bigint NOT NULL
+);
+
+;
+ALTER TABLE "fsoplatjson" ADD CONSTRAINT "fsoplatjson_fk_host_fk" FOREIGN KEY ("host_fk")
+  REFERENCES "host" ("host_id") DEFERRABLE;
+
+;
+
+COMMIT;
+
