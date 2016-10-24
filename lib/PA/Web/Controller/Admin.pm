2dc6a8de0dbd7189d28f6f990dabf1e2fbaf570c
Gordon Marler <gmarler@gmarler.com>
2016-10-24 17:59:29 -0400

    Work on Admin controller - setting up URL namespace

--- lib/PA/Web/Controller/Admin.pm
+++ lib/PA/Web/Controller/Admin.pm
@@ -21,13 +21,39 @@ Catalyst Controller.
 
 =cut
 
-sub index :Path :Args(0) {
-    my ( $self, $c ) = @_;
+#sub index :Path :Args(0) {
+#    my ( $self, $c ) = @_;
+#
+#    $c->response->body('Matched PA::Web::Controller::Admin in Admin.');
+#}
 
-    $c->response->body('Matched PA::Web::Controller::Admin in Admin.');
+sub admin : PathPart('admin') Chained('/') CaptureArgs(0) {
+  my ($self, $c) = @_;
 }
 
+sub hosts : PathPart('hosts') Chained('admin') Args(0) {
+  my ( $self, $c ) = @_;
 
+  my %host_list;
+  my $host_rs = $c->model('DB::Host')->search;
+  while ( my $host_row = $host_rs->next ) {
+    $host_list{ $host_row->name } =
+      { id => $host_row->host_id,
+        time_zone => $host_row->time_zone,
+      };
+  }
+  $c->response->body('Fetched list of hosts to administer.');
+}
+
+sub host : PathPart('host') Chained('admin') CaptureArgs(1) {
+  my ($self, $c ) = @_;
+}
+
+sub delete :PathPart('delete') : Chained('host'): Args(0) {
+  my ($self, $c ) = @_;
+  # TODO: After deleting host, redirect back to host admin page
+  return $c->res->redirect( $c->uri_for('/admin/hosts') );
+}
 
 =encoding utf8
 
