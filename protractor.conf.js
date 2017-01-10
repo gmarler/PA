6a4b3daf2c588f6f2e83ee9bc117c21c647c9f2c
gmarler <gmarler@gmarler.com>
2017-01-10 15:43:15 -0500

    Start work on angular-cli

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,11 +1,32 @@
+// Protractor configuration file, see link for more information
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+
+/*global jasmine */
+var SpecReporter = require('jasmine-spec-reporter');
+
 exports.config = {
-  // chromeOnly: true,
-  framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['root/spec/e2e/spec.js'],
-  jasmineNodeOpts: {
-    showColors: true, // use colors in the command line report
-    defaultTimeoutInterval: 30000
+  allScriptsTimeout: 11000,
+  specs: [
+    './root/spec/e2e/**/*.e2e-spec.ts'
+  ],
+  capabilities: {
+    'browserName': 'chrome'
   },
-  baseUrl: 'http://localhost:8080/'
+  directConnect: true,
+  baseUrl: 'http://localhost:4200/',
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000,
+    print: function() {}
+  },
+  useAllAngular2AppRoots: true,
+  beforeLaunch: function() {
+    require('ts-node').register({
+      project: 'e2e'
+    });
+  },
+  onPrepare: function() {
+    jasmine.getEnv().addReporter(new SpecReporter());
+  }
 };
