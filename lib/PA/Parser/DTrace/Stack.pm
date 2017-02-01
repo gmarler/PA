0baf77a1a340e53487c9e1aaa7adb981f982b87f
Gordon Marler <gmarler@gmarler.com>
2017-02-01 15:17:57 -0500

    Add regex attributes and tests for them

--- lib/PA/Parser/DTrace/Stack.pm
+++ lib/PA/Parser/DTrace/Stack.pm
@@ -10,6 +10,65 @@ use v5.20;
 use Moose;
 use namespace::autoclean;
 
+has 'epoch_interval_regex' => (
+  is         => 'ro',
+  isa        => 'RegexpRef',
+  default    =>
+    sub {
+      qr/ (?<epoch> ^\d+) \n
+          (?<interval_stacks> .+?)
+          (?= (?: ^ \d+ \n | \z ) )
+        /smx;
+    },
+);
+
+has 'datetime_interval_regex' => (
+  is         => 'ro',
+  isa        => 'RegexpRef',
+  default    =>
+    sub {
+      qr{^
+         (?<datetime>
+             \d{4} \s+        # year
+             (?:Jan|Feb|Mar|Apr|May|Jun|
+                Jul|Aug|Sep|Oct|Nov|Dec
+             ) \s+
+             \d+ \s+          # day of month
+             \d+:\d+:\d+ \s+  # HH:MM:DD  (24 hour clock)
+             \n
+         )
+         (?<interval_stacks> .+?)
+         (?=
+           (?:
+             \d{4} \s+        # year
+             (?:Jan|Feb|Mar|Apr|May|Jun|
+                Jul|Aug|Sep|Oct|Nov|Dec
+             ) \s+
+             \d+ \s+          # day of month
+             \d+:\d+:\d+ \s+  # HH:MM:DD  (24 hour clock)
+             \n
+             |
+             \z
+           )
+         )
+        }smx;
+    },
+);
+
+has 'stack_regex' => (
+  is         => 'ro',
+  isa        => 'RegexpRef',
+  default    =>
+    sub {
+      qr{
+         (?<stack> .+?)
+         ^   \s+? (?<stack_count> \d+) \n
+        }smx;
+    },
+);
+
+
+
 
 
 1;
--- t/lib/TF/PA/Parser/DTrace/Stack.pm
+++ t/lib/TF/PA/Parser/DTrace/Stack.pm
@@ -14,16 +14,16 @@ with 'Test::Class::Moose::Role::AutoUse';
 # sub test_startup {
 #   my ($test, $report) = @_;
 #   $test->next::method;
-# 
+#
 #   # Log::Log4perl configuration in a string ...
 #   my $log_conf = q(
 #     logperl.rootLogger              = DEBUG, Screen
-# 
+#
 #     log4perl.Appender.Screen        = Log::Log4perl::Appender::Screen
 #     log4perl.Appender.Screen.stderr = 0
 #     log4perl.Appender.Screen.layout = Log::Log4perl::Layout::SimpleLayout
 #   );
-# 
+#
 #   # ... passed as a reference to init()
 #   Log::Log4perl::init( \$log_conf );
 # }
@@ -33,6 +33,18 @@ sub test_constructor {
 
   my $class = $test->class_name;
   can_ok $class, 'new';
-  isa_ok my $object = $class->new, $class
+  isa_ok my $object = $class->new, $class;
+}
+
+sub test_regexes {
+  my ($test) = shift;
+
+  my $class = $test->class_name;
+  isa_ok my $object = $class->new, $class;
+
+  my (@regexes) = qw/epoch_interval_regex datetime_interval_regex stack_regex/;
+  foreach my $regex_name (@regexes) {
+    isa_ok($object->$regex_name, 'Regexp', "$regex_name exists");
+  }
 }
 
