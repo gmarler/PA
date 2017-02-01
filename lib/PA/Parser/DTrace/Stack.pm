736b31ca68f46aa857be24f9039672c9802a0969
Gordon Marler <gmarler@gmarler.com>
2017-02-01 17:42:17 -0500

    First pass at interval_parse_recurse

--- lib/PA/Parser/DTrace/Stack.pm
+++ lib/PA/Parser/DTrace/Stack.pm
@@ -8,6 +8,9 @@ use v5.20;
 # ABSTRACT: Parser for DTrace stack()/ustack() output variants
 
 use Moose;
+use List::MoreUtils             qw(first_index);
+use Data::Dumper;
+use JSON::MaybeXS               qw(encode_json decode_json);
 use namespace::autoclean;
 
 has 'epoch_interval_regex' => (
@@ -67,6 +70,72 @@ has 'stack_regex' => (
     },
 );
 
+
+=method interval_parse_recurse
+
+Given a raw stack, parse and return each reduced interval.
+
+=cut
+
+sub interval_parse_recurse {
+  my ($self, $raw_stack) = @_;
+
+  # TODO: See which regex matches, then start using that one
+  #
+  my $interval_regex = $self->datetime_interval_regex;
+  my $stack_regex    = $self->stack_regex;
+  my ($match_count);
+
+  while ($raw_stack =~ m{$interval_regex}gsmx) {
+
+    $match_count++;
+    say "DATETIME " . $+{datetime};
+    #say "STACKS   " . $+{interval_stacks};
+
+    my $interval_stacks = $+{interval_stacks};
+    my $interval_href = { name => 'root', value => 0, children => {} };
+    while ($interval_stacks =~ m{$stack_regex}gsmx) {
+      say "STACK:\n$+{stack}";
+      say "STACK_COUNT: $+{stack_count}";
+      my ($stack) = $+{stack};
+      # Tear down stack
+      # - split on newlines
+      # - AND *reverse* to get order right
+      my (@stack) = reverse split /\n/, $stack;
+      # - delete blanks lines at beginning of the array
+      my $index = List::MoreUtils::first_index {$_ ne ''} @stack;
+      if (defined($index)) {
+        splice(@stack,0,$index);
+      }
+      # - delete blanks lines at end of the array
+      #   Apparently unnecessary due to regex above
+      #   NOTE: below is BROKEN - fix if needed
+      #$index = List::MoreUtils::first_index {$_ ne ''} reverse @stack;
+      #if (defined($index)) {
+      #  say "INDEX: $index";
+      #  #splice(@stack,-1,$index);
+      #}
+      foreach my $frame (@stack) {
+        # - Strip off leading whitespace
+        $frame =~ s{^\s+}{};
+        # - strip off +0x.+
+        $frame =~ s{\+.+}{};
+        # - Replace blank line in middle of stack with '-'
+        #   That's the dividing mark between user/kernel stacks
+        $frame =~ s{^$}{\-};
+      }
+      say Dumper(\@stack);
+      my $intermediate =
+        $self->add_recurse($interval_href, \@stack, $+{stack_count});
+      my $serialized =
+        $self->serialize_recurse($intermediate);
+      $Data::Dumper::Indent = 1;
+      say Dumper($serialized);
+      my ($json) = encode_json($serialized);
+    }
+  }
+
+}
 
 =method add_recurse
 
